6ccd81218b12b2e9104387529a0e52575da854c9
mylanng <mylannguyen0421@gmail.com>
2024-03-10 09:01:27 -0700

    jam modify

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
     return (
         <div className='navBar'>
             <button className='navButton'><Link class="a" to={"/home"}> Home </Link></button>
-            <button className='navButton'><Link class="a" to={"/jam"}> Jamsg </Link></button>
+            <button className='navButton'><Link class="a" to={"/jam"}> Jams </Link></button>
             <button className='navButton'><Link class="a" to={"/forum"}> Jam Forum </Link></button>
         </div>
     );
--- my-app/src/pages/Jam.js
+++ my-app/src/pages/Jam.js
@@ -12,14 +12,14 @@ function Jam() {
                 <div class="heading">Locations:</div>
                 <div class="heading"> Tags:</div>
                 <div class="heading">Description:</div>
-                <div class="heading">Members:</div>
+                <div class="heading">Members: </div>
             </div>
 
             <div className="middle_block">
-                <div class="location_text">Vancouver</div>
+                <div class="location_text">Vancouver<br /> </div>
                 <div class= "tags_text">#cyber #security #technology #coding</div>
-                <div class="Description:">This platform is for cyber security lovers. Ideas, projects, and more!</div>
-                <div class= "Members:">Smriti, My Lan, Lavenya, Eknor</div>
+                <div class="description_text">This platform is for cyber security lovers!</div>
+                <div class= "member_text">Christine <br /> Jack <br />Alan<br /></div>
             </div>
 
 
