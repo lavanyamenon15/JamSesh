c3c8074a0bcef9692db314f9aa914398b533e8f9
ekt24 <noortoor1976@gmail.com>
2024-03-10 07:59:40 -0700

    jams done no filter added yet

--- my-app/src/components/JamWidget.js
+++ my-app/src/components/JamWidget.js
@@ -6,10 +6,16 @@ function JamWidget({id, jamName, location, members, tags}) {
     
     return (
         <div className='jamWidget'>
+            <div className='content'>
             <h1 className='jamName'>{jamName} </h1>
-            <p className='jamLocation'> {location} </p>
-            <p className='jamMembers'> Number of Jam Members: {members} </p>
-            <p className='jamTags'> {tags} </p>
+            <p className='jamLocation'> Location:&nbsp;{location} </p>
+            <p className='jamMembers'> # of Jam Members:&nbsp;{members} </p>
+            <div className='jamTags'>
+        {tags.map((tag, index) => (
+            <p className="jamT" key={index}>#{tag}</p>
+        ))}
+    </div>
+    </div>
             <Link to={'/jams/' + id }><button className='jamButton'> Take me to the jam!</button></Link>
         </div>
     );
