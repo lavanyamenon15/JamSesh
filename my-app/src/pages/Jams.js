41353b6fc26c9fc0dba41ebe71c9f43ce7710748
lmenon <lavanyamenon1215@gmail.com>
2024-03-10 10:48:25 -0700

    finished implementing a filter feature

--- my-app/src/pages/Jams.js
+++ my-app/src/pages/Jams.js
@@ -8,6 +8,59 @@ function Jams() {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    const [formData, setFormData] = useState({
+        location: " ",
+        tags: " ",
+      });
+    
+      // Function to handle form input changes
+      const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+          ...formData,
+          [name]: value
+        });
+      };
+    
+      // Function to handle form submission
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        // You can perform form validation or submit the form data here
+        getData();
+        console.log('Form submitted:', formData);
+
+      };
+
+    const filterByLocation = (jams) => {
+        if(formData.location != " "){
+            var newJamsList = []
+            jams.forEach(element => {
+                if(element.location == formData.location){
+                    newJamsList.push(element);
+                }
+            });
+            return newJamsList;
+        } else {
+            return jams;
+        }
+    }
+
+    const filterByTags = (jams) => {
+        if(formData.tags != " "){
+            var newJamsList = []
+            jams.forEach(element => {
+                console.log(element)
+                if(element.tags.includes(formData.tags)){
+                    newJamsList.push(element);
+                }
+            });
+            console.log(newJamsList);
+            return newJamsList;
+        } else {
+            return jams;
+        }
+    }
+
     const getData = async () => {
 
         const requestOptions = {
@@ -17,7 +70,9 @@ function Jams() {
 
         try {
             const response = await fetch("http://localhost:3030/ActiveJams", requestOptions);
-            const result = await response.json();
+            var result = await response.json();
+            result = await filterByLocation(result);
+            result = await filterByTags(result);
             setData(result)
             setLoading(false);
             console.log(result);
@@ -36,8 +91,46 @@ function Jams() {
     }
 
     return (
-        <div>
-        <NavBar></NavBar>
+        <div><NavBar></NavBar>
+        <div className='bigBoard'>
+        <div className='filter'> 
+            <form onSubmit={handleSubmit}>
+            <label>
+                Location:
+                <select
+                type="select"
+                name="location"
+                value={formData.location}
+                onChange={handleInputChange}
+                required
+                >
+                    <option value=" ">Select Option</option>
+                    <option value="Vancouver">Vancouver</option>
+                    <option value="Toronto">Toronto</option>
+                </select>
+            </label>
+            <label>
+                Tags:
+                <select
+                type="select"
+                name="tags"
+                value={formData.tags}
+                onChange={handleInputChange}
+                required
+                >
+                    <option value=" ">Select Option</option>
+                    <option value="TECH">Technoloogy</option>
+                    <option value="FILM">Film</option>
+                    <option value="BOTANY">Botany</option>
+                    <option value="MUSIC">Music</option>
+                    <option value="EDITING">Editing</option>
+                    <option value="MOVIE">Movie</option>
+                </select>
+            </label>
+            <br />
+            <button type="submit">Submit</button>
+            </form>
+        </div>
         <div class="jamBoard">
             {data.map((jam) => (
                 <JamWidget
@@ -48,6 +141,7 @@ function Jams() {
                 tags={jam.tags}>
                 </JamWidget>
             ))}
+        </div>
         </div>
         </div>
     );
