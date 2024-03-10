1f20156ee816bbd1e23dfca8da7e092a3cecebb3
mylanng <mylannguyen0421@gmail.com>
2024-03-10 00:19:43 -0800

    chat feature dev

--- my-app/src/pages/Jam.js
+++ my-app/src/pages/Jam.js
@@ -17,6 +17,9 @@ function Jam() {
 
             <div className="middle_block">
                 <div class="location_text">Vancouver</div>
+                <div class= "tags_text">#cyber #security #technology #coding</div>
+                <div class="Description:">This platform is for cyber security lovers. Ideas, projects, and more!</div>
+                <div class= "Members:">Smriti, My Lan, Lavenya, Eknor</div>
             </div>
 
 
