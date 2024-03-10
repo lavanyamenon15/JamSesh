2f66aca78b72a8cb59ad380474f4afc7832f0701
ekt24 <noortoor1976@gmail.com>
2024-03-10 05:15:37 -0700

    blob update

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import '../css/Home.css'
 import NavBar from '../components/NavBar';
+import { useState } from 'react';
+import { useBlob } from './blob';
 
 
 function Home() {
+    const [canvas, setCanvas] = useState();
+    useBlob(canvas)
     return (
         <div class="main">
             <NavBar></NavBar>
-            <h1 class="head"> Welcome to JAM SESH!</h1>
-            <div class="textbox"><p class="p"> Here, creativity knows no bounds. Whether you're passionate about tech, art, or anything in between, our platform offers a space for sharing and collaborating on project ideas. From coding to crafting, ignite your imagination and connect with like-minded individuals on dedicated topic boards. Join us and let's jam!</p></div>
+            <div class="side">
+            <div>
+                <h1 class="head"> Welcome to JAM SESH!</h1>
+            <div class="textbox"><p class="p"> Here, creativity knows no bounds. Whether you're passionate about tech, art, or anything in between, our platform offers a space for sharing and collaborating on project ideas. From coding to crafting, ignite your imagination and connect with like-minded individuals on dedicated topic boards. Join us and let's jam!</p></div></div>
+            <canvas width={500} height={500} ref={el => setCanvas(el)}/>
+            </div>
             
         </div>
     );
